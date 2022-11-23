0bb4060a79dc7e02185ad05a242b565464d12a91
Joseph <wamiti8711@gmail.com>
2022-11-23 17:57:50 -0500

    functions continued

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+//defines the function as specified
+function saturdayFun(activity='roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+
+saturdayFun()
+
+function mondayWork(office = "go to the office"){
+    return `This Monday, I will ${office}.`
+}
+
+mondayWork()
+
+function wrapAdjective(adjective = "*"){
+
+    return function(message){
+        return `You are ${adjective}${message}${adjective}!`
+    };
+
+}
+wrapAdjective('*')('a hard worker')('*')
+wrapAdjective('||')("a dedicated programmer")('||')
